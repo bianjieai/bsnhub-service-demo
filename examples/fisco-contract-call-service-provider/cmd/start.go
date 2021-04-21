ea52e740950e7e3f0a2eecc9ac258d7801ef0c9e
王迪 <948200496@qq.com>
2021-04-21 14:41:42 +0800

    update mysql to add field source_service (#6)
    Co-authored-by: 王迪 <wangdi@bianjie.ai>

--- examples/fisco-contract-call-service-provider/cmd/start.go
+++ examples/fisco-contract-call-service-provider/cmd/start.go
@@ -52,7 +52,7 @@ func StartCmd() *cobra.Command {
 
 			mysqlConfig := mysql.NewConfig(config)
 			mysql.NewDB(mysqlConfig)
-			defer mysql.DB.Close()
+			defer mysql.Close()
 
 			go server.StartWebServer(chainManager)
 			appInstance.Start()
